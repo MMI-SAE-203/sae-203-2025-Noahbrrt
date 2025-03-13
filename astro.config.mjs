e0c32f13b75518f574a4c1f99c0da24caeee4f04
Noahbrrt <noah.barret@edu.univ-fcomte.fr>
2025-03-13 13:47:47 +0100

    backend

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,19 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 
+import netlify from '@astrojs/netlify';
+
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
+
   experimental: {
     svg: true,
   },
-  output: 'server'
+
+  output: 'server',
+  adapter: netlify()
 });
